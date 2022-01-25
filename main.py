65bb106cf074cbb25610871ed1d83499b98c63d1
Kirleoke <shalugin.kirillkpc2442020@gmail.com>
2022-01-25 23:04:20 +0300

    Решение квадратных уравнений

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = "+str(x1)+" x2 = "+str(x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = "+str(x))
+else:
+    print("Корней нет")
